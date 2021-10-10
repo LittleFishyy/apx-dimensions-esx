3a0700ba69e63bc3242a5b6055999f2b1798e664
BombayV <OMGmsebastian@gmail.com>
2021-10-10 13:41:14 -0400

    refactor(core): change to use lua54 + server_only

--- Server/SMain.lua
+++ Server/SMain.lua
@@ -1,21 +1,25 @@
 AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-        return
+    if (GetCurrentResourceName() == resourceName) then
+        TriggerClientEvent('chat:addSuggestion', -1, '/'..Config.ChangeDimension, _U('changedimension_help'), {
+            { name = _U('param_1'), help = _U('param_description_1') },
+            { name = _U('param_2'), help = _U('param_description_2') }
+        })
+        return print(_U('resource_started', resourceName))
     end
-    print(_U('resource_started', resourceName))
 end)
 
 RegisterCommand(Config.ChangeDimension, function(source, args)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local playerId = tonumber(args[1])
+    local playerId <const> = source
+    local xPlayer = ESX.GetPlayerFromId(playerId)
+    local targetId = tonumber(args[1])
     local dimension = tonumber(args[2])
-    local xTarget = ESX.GetPlayerFromId(playerId)
+    local xTarget = ESX.GetPlayerFromId(targetId)
 
     if xPlayer.getGroup() == 'admin' then
         if args[1] and tonumber(args[1]) and xTarget then
             if args[2] and tonumber(args[2]) then
-                SetPlayerRoutingBucket(playerId, dimension)
-                xPlayer.showNotification(_U('changed_dimension', GetPlayerName(playerId), dimension))
+                SetPlayerRoutingBucket(targetId, dimension)
+                xPlayer.showNotification(_U('changed_dimension', GetPlayerName(targetId), dimension))
             else
                 xPlayer.showNotification(_U('not_dimension'))
             end
@@ -29,15 +33,17 @@ end)
 
 -- Autofix Command --
 RegisterCommand(Config.AutoFixCommand, function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local playerId <const> = source
+    local xPlayer = ESX.GetPlayerFromId(playerId)
 
     xPlayer.showNotification(_U('dimension_set_0'))
-    SetPlayerRoutingBucket(source, 0)
+    SetPlayerRoutingBucket(playerId, 0)
 end)
 
 -- View dimension Command --
 RegisterCommand(Config.ViewDimension, function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local playerId <const> = source
+    local xPlayer = ESX.GetPlayerFromId(playerId)
 
-    xPlayer.showNotification(_U('view_dimension', GetPlayerRoutingBucket(source)))
+    xPlayer.showNotification(_U('view_dimension', GetPlayerRoutingBucket(playerId)))
 end)
